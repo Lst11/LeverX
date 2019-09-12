d40846a6510ccda844d24373de857a7d59d76f60
lst11 <stanishevskaya.lena@gmail.com>
2019-09-12 13:45:09 +0300

    db properties

--- src/main/java/com/leverx/config/DataBaseConfig.java
+++ src/main/java/com/leverx/config/DataBaseConfig.java
@@ -1,5 +1,7 @@
 package com.leverx.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -8,6 +10,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 @Configuration
 @EnableJpaRepositories("com.leverx.repository")
@@ -18,6 +21,16 @@ public class DataBaseConfig {
 
     @Resource
     private Environment env;
+
+    @Bean
+    public DataSource dataSource(){
+        BasicDataSource dataSource = new BasicDataSource();
+        dataSource.setUrl(env.getRequiredProperty("db.url"));
+        dataSource.setDriverClassName(env.getRequiredProperty("db.driver"));
+        dataSource.setUsername(env.getRequiredProperty("db.username"));
+        dataSource.setPassword(env.getRequiredProperty("db.password"));
+        return dataSource;
+    }
 
 
 
