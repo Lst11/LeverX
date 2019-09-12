41cc0d3f43adb1432bc7dafae26149498cdd0cbf
lst11 <stanishevskaya.lena@gmail.com>
2019-09-12 14:44:34 +0300

    json converter

--- src/main/java/com/leverx/config/WebConfig.java
+++ src/main/java/com/leverx/config/WebConfig.java
@@ -1,13 +1,27 @@
 package com.leverx.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Collections;
+import java.util.List;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.leverx")
 public class WebConfig implements WebMvcConfigurer {
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        MappingJackson2HttpMessageConverter customConverter = new MappingJackson2HttpMessageConverter();
+        customConverter.setObjectMapper(new ObjectMapper());
+        customConverter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        converters.add(customConverter);
+    }
 }
--- src/main/java/com/leverx/controller/UserController.java
+++ src/main/java/com/leverx/controller/UserController.java
@@ -1,18 +1,27 @@
 package com.leverx.controller;
 
-import org.springframework.stereotype.Controller;
+import com.leverx.entity.Book;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+
+@RestController
 @RequestMapping("/users")
 public class UserController {
 
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     @ResponseBody
-    public String gerUser(ModelMap modelMap) {
-        return "My User";
+    public Book gerUser() {
+        return createMockRemind();
+    }
+
+    private Book createMockRemind() {
+        Book book = new Book();
+        book.setId(1);
+        book.setName("Super name");
+        return book;
     }
 }
